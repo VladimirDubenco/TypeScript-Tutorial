e503199740d5a88307f7fe2a784b40830ebc5b50
vladimirdubenco <vladimirdubenco@gmail.com>
2022-12-01 12:50:09 +0200

    first commit

---
+++ hello.ts
@@ -0,0 +1,14 @@
+//Tis is a comment
+
+//TODO: This is todo
+
+//FIXME: This is fix me!
+
+/**
+ * This is a multiline comment
+ * this is another line of comment
+ */
+
+
+console.log('Hello World!');
+
